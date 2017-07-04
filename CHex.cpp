43c9a724132111b152803e1ad6ca65f93254b585
larry <china_larry@163.com>
2017-07-04 21:48:57 +0800

    Set函数P1公共区

--- CHex.cpp
+++ CHex.cpp
@@ -10,7 +10,377 @@ unsigned short CHex::GetProjectCount()
     return m_usProjectCount;
 }
 
+QString CHex::GetrBatchPreNum()
+{
+    return m_strBatchPreNum;
+}
+
+unsigned short CHex::GetBarcodeFlag()
+{
+    return m_usBarcodeFlag;
+}
+
+unsigned short CHex::GetBarcodeNum()
+{
+    return m_usBarcodeNum;
+}
+
+unsigned short CHex::GetStandardCount()
+{
+    return m_usStandardCount;
+}
+
+unsigned short CHex::GetTemperatureFlag()
+{
+    return m_usTemperatureFlag;
+}
+
+unsigned short CHex::GetAreaUseFlag()
+{
+    return m_usAreaUseFlag;
+}
+
+QString CHex::GetrAreaUseName()
+{
+    return m_strAreaUseName;
+}
+
+QString CHex::GetrTitleName()
+{
+    return m_strTitleName;
+}
+
+QString CHex::GetrUnitName()
+{
+    return m_strUnitName;
+}
+
+unsigned short CHex::GetDecimalsDigit()
+{
+    return m_usDecimalsDigit;
+}
+
+unsigned short CHex::GetCountDownTime()
+{
+    return m_usCountDownTime;
+}
+
+unsigned short CHex::GetTestTime()
+{
+    return m_usTestTime;
+}
+
+unsigned short CHex::GetInstrumentType()
+{
+    return m_usInstrumentType;
+}
+
+unsigned short CHex::GetTCTestValue()
+{
+    return m_usTCTestValue;
+}
+
+unsigned short CHex::GetTCMolecularValue()
+{
+    return m_usTCMolecularValue;
+}
+
+unsigned short CHex::GetTCDenominatorValue()
+{
+    return m_usTCDenominatorValue;
+}
+
+double CHex::GetSerumValue()
+{
+    return m_dSerumValue;
+}
+
+double CHex::GetWholeBloodValue()
+{
+    return m_dWholeBloodValue;
+}
+
+double CHex::GetUrineValue()
+{
+    return m_dUrineValue;
+}
+
+double CHex::GetExcrementValue()
+{
+    return m_dExcrementValue;
+}
+
+double CHex::GetQualityLiquidValue()
+{
+    return m_dQualityLiquidValue;
+}
+
+double CHex::GetACoefficientValue()
+{
+    return m_dACoefficientValue;
+}
+
+double CHex::GetBCoefficientValue()
+{
+    return m_dBCoefficientValue;
+}
+
+double CHex::GetA1CoefficientValue()
+{
+    return m_dA1CoefficientValue;
+}
+
+double CHex::GetB1CoefficientValue()
+{
+    return m_dB1CoefficientValue;
+}
+
+unsigned short *CHex::GetPeakName()
+{
+    return m_usPeakName;
+}
+
+unsigned short *CHex::GetPeakBeginArray()
+{
+    return m_usPeakBeginArray;
+}
+
+unsigned short *CHex::GetzPeankEndArray()
+{
+    return m_usPeankEndArray;
+}
+
+unsigned short *CHex::GetPeakValueNumArray()
+{
+    return m_usPeakValueNumArray;
+}
+
+unsigned short *CHex::GetPeakXBeginArray()
+{
+    return m_usPeakXBeginArray;
+}
+
+unsigned short *CHex::GetPeankXEndArray()
+{
+    return m_usPeankXEndArray;
+}
+
+unsigned short *CHex::GetPeakXValueNumArray()
+{
+    return m_usPeakXValueNumArray;
+}
+
+double *CHex::GetRangMinArray()
+{
+    return m_dRangMinArray;
+}
+
+double *CHex::GetRangMaxArray()
+{
+    return m_dRangMaxArray;
+}
+
+QString *CHex::GetProjectNameArray()
+{
+    return m_strProjectNameArray;
+}
+
+unsigned short CHex::GetPrintInfoCount()
+{
+    return m_usPrintInfoCount;
+}
+
+QString CHex::GetPrintInfo()
+{
+    return m_strPrintInfo;
+}
+///////////////////////////////////////////////////////////////////////////////////
 void CHex::SetProjectCount(unsigned short usProjectCount)
 {
     m_usProjectCount = usProjectCount;
+}
+
+void CHex::SetrBatchPreNum(QString strBatchPreNum)
+{
+    m_strBatchPreNum = strBatchPreNum;
+}
+
+void CHex::SetBarcodeFlag(unsigned short usBarcodeFlag)
+{
+    m_usBarcodeFlag = usBarcodeFlag;
+}
+
+void CHex::SetBarcodeNum(unsigned short usBarcodeNum)
+{
+    m_usBarcodeNum = usBarcodeNum;
+}
+
+void CHex::SetStandardCount(unsigned short usStandardCount)
+{
+    m_usStandardCount = usStandardCount;
+}
+
+void CHex::SetTemperatureFlag(unsigned short usTemperatureFlag)
+{
+    m_usTemperatureFlag = usTemperatureFlag;
+}
+
+void CHex::SetAreaUseFlag(unsigned short usAreaUseFlag)
+{
+    m_usAreaUseFlag = usAreaUseFlag;
+}
+
+void CHex::SetrAreaUseName(QString strAreaUseName)
+{
+    m_strAreaUseName = strAreaUseName;
+}
+
+void CHex::SetrTitleName(QString strTitleName)
+{
+    m_strTitleName = strTitleName;
+}
+
+void CHex::SetrUnitName(QString strUnitName)
+{
+    m_strUnitName = strUnitName;
+}
+
+void CHex::SetDecimalsDigit(unsigned short usDecimalsDigit)
+{
+    m_usDecimalsDigit = usDecimalsDigit;
+}
+
+void CHex::SetCountDownTime(unsigned shortusCountDownTime)
+{
+    m_usCountDownTime = shortusCountDownTime;
+}
+
+void CHex::SetTestTime(unsigned short usTestTime)
+{
+    m_usTestTime = usTestTime;
+}
+
+void CHex::SetInstrumentType(unsigned short usInstrumentType)
+{
+    m_usInstrumentType = usInstrumentType;
+}
+
+void CHex::SetTCTestValue(unsigned short usTCTestValue)
+{
+    m_usTCTestValue = usTCTestValue;
+}
+
+void CHex::SetTCMolecularValue(unsigned short usTCMolecularValue)
+{
+    m_usTCMolecularValue = usTCMolecularValue;
+}
+
+void CHex::SetTCDenominatorValue(unsigned short usTCDenominatorValue)
+{
+    m_usTCDenominatorValue = usTCDenominatorValue;
+}
+
+void CHex::SetSerumValue(double dSerumValue)
+{
+    m_dSerumValue = dSerumValue;
+}
+
+void CHex::SetWholeBloodValue(double dWholeBloodValue)
+{
+    m_dWholeBloodValue = dWholeBloodValue;
+}
+
+void CHex::SetUrineValue(double dUrineValue)
+{
+    m_dUrineValue = dUrineValue;
+}
+
+void CHex::SetExcrementValue(double dExcrementValue)
+{
+    m_dExcrementValue = dExcrementValue;
+}
+
+void CHex::SetQualityLiquidValue(double dQualityLiquidValue)
+{
+    m_dQualityLiquidValue = dQualityLiquidValue;
+}
+
+void CHex::SetACoefficientValue(double dACoefficientValue)
+{
+    m_dACoefficientValue = dACoefficientValue;
+}
+
+void CHex::SetBCoefficientValue(double dBCoefficientValue)
+{
+    m_dBCoefficientValue = dBCoefficientValue;
+}
+
+void CHex::SetA1CoefficientValue(double dA1CoefficientValue)
+{
+    m_dA1CoefficientValue = dA1CoefficientValue;
+}
+
+void CHex::SetB1CoefficientValue(double dB1CoefficientValue)
+{
+    m_dB1CoefficientValue = dB1CoefficientValue;
+}
+
+void CHex::SetPeakName(unsigned short *usPeakName)
+{
+    memcpy(m_usPeakName, usPeakName, 3);
+}
+
+void CHex::SetPeakBeginArray(unsigned short *usPeakBeginArray)
+{
+    memcpy(m_usPeakBeginArray, usPeakBeginArray, 3);
+}
+
+void CHex::SetzPeankEndArray(unsigned short *usPeankEndArray)
+{
+    memcpy(m_usPeankEndArray, usPeankEndArray, 3);
+}
+
+void CHex::SetPeakValueNumArray(unsigned short *usPeakValueNumArray)
+{
+    memcpy(m_usPeakValueNumArray, usPeakValueNumArray, 3);
+}
+
+void CHex::SetPeakXBeginArray(unsigned short *usPeakXBeginArray)
+{
+    memcpy(m_usPeakXBeginArray, usPeakXBeginArray, 4);
+}
+
+void CHex::SetPeankXEndArray(unsigned short *usPeankXEndArray)
+{
+    memcpy(m_usPeankXEndArray, usPeankXEndArray, 4);
+}
+
+void CHex::SetPeakXValueNumArray(unsigned short *usPeakXValueNumArray)
+{
+    memcpy(m_usPeakXValueNumArray, usPeakXValueNumArray, 4);
+}
+
+void CHex::SetRangMinArray(double *dRangMinArray)
+{
+    memcpy(m_dRangMinArray, dRangMinArray, 5);
+}
+
+void CHex::SetRangMaxArray(double *dRangMaxArray)
+{
+    memcpy(m_dRangMaxArray, dRangMaxArray, 5);
+}
+
+void CHex::SetProjectNameArray(QString *strProjectNameArray)
+{
+    memcpy(m_strProjectNameArray, strProjectNameArray, 5);
+}
+
+void CHex::SetPrintInfoCount(unsigned short usPrintInfoCount)
+{
+    m_usPrintInfoCount = usPrintInfoCount;
+}
+
+void CHex::SetPrintInfo(QString strPrintInfo)
+{
+    m_strPrintInfo = strPrintInfo;
 }
--- CHex.h
+++ CHex.h
@@ -10,10 +10,107 @@ public:
 
 public:
     //  项目1公共数据区
-    unsigned short GetProjectCount();
-
+    unsigned short GetProjectCount();// 项目数
+    QString GetrBatchPreNum();// 批号前缀
+    unsigned short GetBarcodeFlag();// 条码标识
+    unsigned short GetBarcodeNum();// 条码号
+    unsigned short GetStandardCount();// 标准点数
+    unsigned short GetTemperatureFlag();// 是否温度补偿
+    unsigned short GetAreaUseFlag();// 是否设定区域
+    QString GetrAreaUseName();// 区域名称
+    QString GetrTitleName();// 报告单标题】
+    QString GetrUnitName();// 单位名称
+    unsigned short GetDecimalsDigit();// 小数点位
+    unsigned short GetCountDownTime();// 倒计时时间
+    unsigned short GetTestTime();// 测试时间
+    unsigned short GetInstrumentType();// 仪器类型
+    // 测试值
+    unsigned short GetTCTestValue();// TC公式 0-7
+    unsigned short GetTCMolecularValue();// TC分子，1-4
+    unsigned short GetTCDenominatorValue();// TC分母,1-
+    //
+    double GetSerumValue();// 血清血浆系数
+    double GetWholeBloodValue();// 全血系数
+    double GetUrineValue();// 尿液系数
+    double GetExcrementValue();// 粪便系数
+    double GetQualityLiquidValue();// 质控液系数
+    double GetACoefficientValue();// a系数
+    double GetBCoefficientValue();// b系数
+    double GetA1CoefficientValue();// a1系数
+    double GetB1CoefficientValue();// b1系数
+    // 单项目峰值名称
+    unsigned short* GetPeakName();
+    // 单项目峰值开始位置
+    unsigned short* GetPeakBeginArray();
+    // 单项目峰值结束位置
+    unsigned short* GetzPeankEndArray();
+    // 单项目峰值取值数量
+    unsigned short* GetPeakValueNumArray();
+    // 多项目峰值开始位置
+    unsigned short* GetPeakXBeginArray();
+    // 多项目峰值结束位置
+    unsigned short* GetPeankXEndArray();
+    // 多项目峰值取值数量
+    unsigned short* GetPeakXValueNumArray();
+    // 检测范围
+    double* GetRangMinArray();
+    double* GetRangMaxArray();
+    // 项目名称
+    QString* GetProjectNameArray();
+    // 打印信息
+    unsigned short GetPrintInfoCount();
+    QString GetPrintInfo();// 倒叙打印
     // 项目1公共数据区
     void SetProjectCount(unsigned short usProjectCount);
+    void SetrBatchPreNum(QString strBatchPreNum);// 批号前缀
+    void SetBarcodeFlag(unsigned short usBarcodeFlag);// 条码标识
+    void SetBarcodeNum(unsigned short usBarcodeNum);// 条码号
+    void SetStandardCount(unsigned short usStandardCount);// 标准点数
+    void SetTemperatureFlag(unsigned short usTemperatureFlag);// 是否温度补偿
+    void SetAreaUseFlag(unsigned short usAreaUseFlag);// 是否设定区域
+    void SetrAreaUseName(QString strAreaUseName);// 区域名称
+    void SetrTitleName(QString strTitleName);// 报告单标题】
+    void SetrUnitName(QString strUnitName);// 单位名称
+    void SetDecimalsDigit(unsigned short usDecimalsDigit);// 小数点位
+    void SetCountDownTime(unsigned shortusCountDownTime);// 倒计时时间
+    void SetTestTime(unsigned short usTestTime);// 测试时间
+    void SetInstrumentType(unsigned short usInstrumentType);// 仪器类型
+    // 测试值
+    void SetTCTestValue(unsigned short usTCTestValue);// TC公式 0-7
+    void SetTCMolecularValue(unsigned short usTCMolecularValue);// TC分子，1-4
+    void SetTCDenominatorValue(unsigned short usTCDenominatorValue);// TC分母,1-
+    //
+    void SetSerumValue(double dSerumValue);// 血清血浆系数
+    void SetWholeBloodValue(double dWholeBloodValue);// 全血系数
+    void SetUrineValue(double dUrineValue);// 尿液系数
+    void SetExcrementValue(double dExcrementValue);// 粪便系数
+    void SetQualityLiquidValue(double dQualityLiquidValue);// 质控液系数
+    void SetACoefficientValue(double dACoefficientValue);// a系数
+    void SetBCoefficientValue(double dBCoefficientValue);// b系数
+    void SetA1CoefficientValue(double dA1CoefficientValue);// a1系数
+    void SetB1CoefficientValue(double dB1CoefficientValue);// b1系数
+    // 单项目峰值名称
+    void SetPeakName(unsigned short *usPeakName);
+    // 单项目峰值开始位置
+    void SetPeakBeginArray(unsigned short *usPeakBeginArray);
+    // 单项目峰值结束位置
+    void SetzPeankEndArray(unsigned short *usPeankEndArray);
+    // 单项目峰值取值数量
+    void SetPeakValueNumArray(unsigned short *usPeakValueNumArray);
+    // 多项目峰值开始位置
+    void SetPeakXBeginArray(unsigned short *usPeakXBeginArray);
+    // 多项目峰值结束位置
+    void SetPeankXEndArray(unsigned short *usPeankXEndArray);
+    // 多项目峰值取值数量
+    void SetPeakXValueNumArray(unsigned short *usPeakXValueNumArray);
+    // 检测范围
+    void SetRangMinArray(double* dRangMinArray);
+    void SetRangMaxArray(double* dRangMaxArray);
+    // 项目名称
+    void SetProjectNameArray(QString* strProjectNameArray);
+    // 打印信息
+    void SetPrintInfoCount(unsigned short usPrintInfoCount);
+    void SetPrintInfo(QString strPrintInfo);// 倒叙打印
 private:
     // 项目1公共数据区
     unsigned short m_usProjectCount;// 项目数
@@ -34,6 +131,7 @@ private:
     unsigned short m_usTCTestValue;// TC公式 0-7
     unsigned short m_usTCMolecularValue;// TC分子，1-4
     unsigned short m_usTCDenominatorValue;// TC分母,1-4
+    //
     double m_dSerumValue;// 血清血浆系数
     double m_dWholeBloodValue;// 全血系数
     double m_dUrineValue;// 尿液系数
